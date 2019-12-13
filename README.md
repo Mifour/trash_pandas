389359129607cf03ea1d1a01e36f61b040ea0bbd
Mifour <dufour.thomas@hotmail.fr>
2019-12-13 11:07:15 +0100

    fix missing module

--- README.md
+++ README.md
@@ -5,15 +5,19 @@ Trahs_pandas is an analytic tool designed for Pandas DataFrame exploration.
 
 It deliver fast statistical insights about the data of every column such as type, NaN values, unique values, distribution, mean, median, quartiles, correlations... and render with graphs using only print statements, making it suitable for both notebook and shell usage.  
 
-It is intended to replace info() and describe().  
+It is intended to replace info() and describe().   
+
+See the code on this [Github](https://github.com/Mifour/trash_pandas)
 
 ### Installation  
 [Pypi link](https://pypi.org/project/trash-pandas/)
 ```bash
 pip install trash-pandas
 ```
+---
 
-### usage
+### usage  
+How to use?
 ```python
 from trash_pandas import explore
 
@@ -22,7 +26,6 @@ df = pandas.read_csv('file.csv')
 explore(df)
 
 ```
-----
 ### yield option  
 It is possible to get a generator that display the result one column at a time. 
 
--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-import setuptools
+from setuptools import setup, Extension, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="trash_pandas", # Replace with your own username
-    version="0.3",
+    version="0.2.2",
     author="Mifour",
     author_email="mifour@yopmail.com",
     description="Analytic tool for Pandas DataFrame exploration",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Mifour/trash_pandas/archive/0.2.tar.gz",
-    packages=setuptools.find_packages(),
+    url="https://github.com/Mifour/trash_pandas/archive/0.2.1.tar.gz",
+    packages=find_packages(),
+    py_modules=['trash_pandas'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ],
     python_requires='>=3',
 )
